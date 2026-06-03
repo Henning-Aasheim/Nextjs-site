dfe28e5cbb51d61cf813d45838a68971c7b3c6bd
Henning-Aasheim <henniner@gmail.com>
2026-06-03 20:57:36 +0200

    Styled the warning correctly

--- components/warning.tsx
+++ components/warning.tsx
@@ -1,13 +1,18 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
+import { TriangleAlert } from 'lucide-react';
 
-export default function Header() {
+export default function Warning() {
     const t = useTranslations('warning');
 
     return (
-        <div className='bg-yellow-400 border-b-2 border-black'>
-            <h1 className='font-bold py-4 mx-auto'>{t('warning')}</h1>
+        <div className='sticky top-0 z-20 w-full bg-yellow-400 border-b-2 border-black'>
+            <h1 className='font-bold py-4 text-xl text-center text-black'> 
+                <TriangleAlert size={22} className='inline-block mr-4' />
+                {t('warning')}
+                <TriangleAlert size={22} className='inline-block ml-4' />
+            </h1>
         </div>
     );
 }
