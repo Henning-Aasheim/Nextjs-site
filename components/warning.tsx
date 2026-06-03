1c64dcaae15d396ad18985c3c92055371f8943f9
Henning-Aasheim <henniner@gmail.com>
2026-06-03 21:04:32 +0200

    A bit better styling for mobile screens (for the warning)

--- components/warning.tsx
+++ components/warning.tsx
@@ -8,7 +8,7 @@ export default function Warning() {
 
     return (
         <div className='sticky top-0 z-20 w-full bg-yellow-400 border-b-2 border-black'>
-            <h1 className='font-bold py-4 text-xl text-center text-black'> 
+            <h1 className='font-bold py-4 text-sm xs:text-lg sm:text-xl text-center text-black'> 
                 <TriangleAlert size={22} className='inline-block mr-4' />
                 {t('warning')}
                 <TriangleAlert size={22} className='inline-block ml-4' />
