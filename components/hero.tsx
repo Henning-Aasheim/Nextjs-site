3f0d46602b7e7d79f2385b3f4db3e6ec5c9a5b9f
Henning Åsheim <henniner@gmail.com>
2026-06-24 12:15:05 +0200

    Styling changes to the hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -15,11 +15,11 @@ export default function Hero({ t }: { t: (key: string) => string }) {
         </div>
 
         {/* Text side */}
-        <div className="w-full lg:w-1/2 order-2 md:order-1 flex items-center ">
-          <div className="px-6 lg:px-10 py-8 lg:py-0 space-y-4 max-w-160 mx-auto lg:mx-0" >
+        <div className="lg:relative w-full lg:w-1/2 order-2 md:order-1 flex items-center mb-2">
+          <div className="lg:absolute lg:right-6 lg:ml-8 lg:mb-2 px-6 lg:px-0 py-8 lg:py-0 space-y-4 max-w-160 mx-auto lg:mx-0" >
             <h1 className="nameLogo dark:text-white text-[clamp(2rem,9vw,5rem)] lg:text-[clamp(2rem,5vw,5rem)]">{t('header1')}</h1>
 
-            <h1 className="nameLogo ml-4 text-orange dark:text-gold italic text-[clamp(2rem,9vw,5rem)] lg:text-[clamp(2rem,5vw,5rem)]">{t('header2')}</h1>
+            <h1 className="nameLogo ml-4 text-orange dark:text-gold text-[clamp(2rem,9vw,5rem)] lg:text-[clamp(2rem,5vw,5rem)]">{t('header2')}</h1>
 
             <p className="dropcap text-[clamp(1rem,1.5vw,1.25rem)]">{t('description')}</p>
           </div>
