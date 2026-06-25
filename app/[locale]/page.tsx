6f1aa5844c740d73753412730f573e03730da371
Henning-Aasheim <henniner@gmail.com>
2026-06-25 09:40:38 +0200

    Major changes to the grid and blog div, smaller changes to hero.

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -30,46 +30,36 @@ export default function Home({ params }: { params: Promise<{ locale: string, dat
     <div className="w-full">
       <div className="grid grid-cols-1 lg:grid-cols-6 grid-rows-15 lg:grid-rows-9 gap-10 max-w-[1200px] mx-auto">
 
-        <div className="lg:col-span-6 row-span-3 bg-darkBlue">
+        <div className="col-span-1 lg:col-span-6 lg:row-span-3 bg-darkBlue">
           <Hero t={t} />
         </div>
 
-        <div className="lg:col-span-4 row-span-3 row-start-4">
+        <div className="col-span-1 lg:col-span-4 col-start-1 lg:row-span-3 lg:row-start-4 bg-orange">
 
           {/* Blog div */}
 
-          <div className="bg-orange dark:bg-darkAccent text-gold py-10">
+          <div className="dark:bg-darkAccent text-lightBg py-10 w-10/11 mx-auto">
 
-            <h1 className="text-2xl sm:text-4xl mb-6 pb-3 font-default font-bold w-4/5 
-                          md:w-1/2 xl:w-0.4 mx-auto border-gold border-solid border-b">{t('blog')}</h1>
+            <h1 className="text-2xl sm:text-4xl pb-3 font-default font-bold mx-auto border-lightBg border-solid border-b">{t('blog')}</h1>
 
-            <div className="my-12">
-              <ul className="grid grid-cols-1 gap-6 w-full items-start text-left">
+            <div className="w-full overflow-hidden">
+              <ul className="grid grid-cols-1 w-full items-start text-left">
                 {visibleArticles.map((article, index) => {
                   const displayNumber = totalArticles - index;
                   const dateTime = new Date(article.date);
 
                   return (
-                    <li
-                      key={article.id}
-                      className="group w-4/5 2xl:w-1/2 mx-auto mb-2 border-b border-gray-700 pb-4"
-                    >
-                      <Link
-                        href={`/blog/${article.id}`}
-                        className="flex w-full items-start gap-6"
-                      >
-                        {/* Number */}
-                        <div className="text-2xl font-mono text-gray-500 w-8 text-right">
-                          { displayNumber }
-                        </div>
+                    <li key={article.id} className="group w-full mx-auto border-b border-lightBg py-4 hover:bg-orangeDark">
+                      <Link href={`/blog/${article.id}`} className="flex w-full items-start gap-6">
 
                         {/* Main content */}
                         <div className="flex-1">
                           {/* Meta: date + category */}
-                          <div className="mb-1 flex flex-wrap items-center gap-2 text-sm text-gray-400">
+                          <div className="mb-1 flex items-center gap-2 text-sm text-lightBg overflow-hidden">
+                            <span className="min-w-[2rem] text-2xl font-display text-lightBg text-center">{ displayNumber }</span>
                             <span className="min-w-[9rem]">{format.dateTime(dateTime, { dateStyle: 'long' })}</span>
-                            <span className="min-w-[7rem] uppercase tracking-wide text-xs font-semibold text-indigo-400">{article.category}</span>
-                            <h2 className="text-lg sm:text-2xl font-default font-semibold mb-1">{article.title}</h2>
+                            <span className="min-w-[7rem] uppercase tracking-wide text-xs font-semibold text-lightText">{article.category}</span>
+                            <h2 className="min-w[9rem] text-lg sm:text-lg font-default font-semibold mb-1 text-wrap">{article.title}</h2>
                           </div>
 
                           {/* Title */}
@@ -84,9 +74,11 @@ export default function Home({ params }: { params: Promise<{ locale: string, dat
           </div>
         </div>
 
-        <div className="lg:col-span-2 row-span-3 lg:col-start-5 row-start-6 lg:row-start-4 bg-darkBlue">9</div>
-        <div className="lg:col-span-3 row-span-3 row-start-9 lg:row-start-7">10</div>
-        <div className="lg:col-span-3 row-span-3 lg:col-start-4 row-start-12 lg:row-start-7">11</div>
+        <div className="col-span-1 lg:col-span-2 lg:row-span-3 col-start-1 lg:col-start-5 lg:row-start-4 bg-darkBlue">
+          
+        </div>
+        <div className="col-span-1 lg:col-span-3 lg:row-span-3 lg:row-start-7 bg-orange">10</div>
+        <div className="col-span-1 lg:col-span-3 lg:row-span-3 col-start-1 lg:col-start-4 lg:row-start-7 bg-orange">11</div>
 
       </div>
     </div>  
--- components/hero.tsx
+++ components/hero.tsx
@@ -2,29 +2,37 @@ import Image from 'next/image';
 
 export default function Hero({ t }: { t: (key: string) => string }) {
   return (
-    <div className="md:mx-auto bg-darkBlue">
-      <div className="relative flex flex-col lg:flex-row min-h-[40vh]">
-
+    <div className="lg:mx-auto bg-darkBlue">
+      <div className="relative flex flex-col lg:block min-h-[40vh]">
         {/* Image side */}
-        <div className="w-full lg:w-1/2 order-1 lg:order-2 ml-auto flex">
+        <div className="w-full lg:w-[65%] ml-auto">
           <Image
             src="/profile_image.jpg"
             alt="Profile Image"
-            width={800}  height={1000}
-            className="mask-b-from-20% lg:mask-l-from-50% lg:mask-b-from-100% w-full h-auto object-cover" />
+            width={800}
+            height={1000}
+            className="mask-b-from-20% lg:mask-l-from-50% lg:mask-b-from-100% w-full h-auto object-cover"
+          />
         </div>
 
         {/* Text side */}
-        <div className="lg:relative w-full lg:w-1/2 order-2 md:order-1 flex items-center mb-2">
-          <div className="lg:absolute lg:right-6 lg:ml-8 lg:mb-2 px-6 lg:px-0 py-8 lg:py-0 space-y-4 max-w-160 mx-auto lg:mx-0" >
-            <h1 className="nameLogo text-lightBg text-[clamp(2rem,9vw,5rem)] lg:text-[clamp(2rem,5vw,5rem)]">{t('header1')}</h1>
+        <div className="w-full lg:w-1/2 lg:absolute lg:top-1/2 lg:left-0 lg:-translate-y-1/2">
+          <div className="px-6 lg:px-8 py-8 space-y-4 max-w-160 mx-auto">
+            <h1 className="nameLogo text-lightBg text-[clamp(2rem,9vw,5rem)] lg:text-[clamp(2rem,5vw,5rem)]">
+              {t('header1')}
+            </h1>
 
-            <h1 className="nameLogo ml-4 text-orange dark:text-gold text-[clamp(2rem,9vw,5rem)] lg:text-[clamp(2rem,5vw,5rem)]">{t('header2')}</h1>
+            <h1 className="nameLogo ml-4 text-orange dark:text-gold text-[clamp(2rem,9vw,5rem)] lg:text-[clamp(2rem,5vw,5rem)]">
+              {t('header2')}
+            </h1>
 
-            <p className="dropcap text-[clamp(1rem,1.5vw,1.25rem)]">{t('description')}</p>
+            <p className="dropcap text-[clamp(1rem,1.5vw,1.25rem)]">
+              {t('description')}
+            </p>
           </div>
         </div>
       </div>
     </div>
+
   );
 }
